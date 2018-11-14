3988dd3c7296517435f05f4196869ff708f24656
emilioeigler <emilioeigler@hotmail.com>
2018-11-14 09:21:32 -0300

    added laser

--- Position2D.gd
+++ Position2D.gd
@@ -1,13 +1,15 @@
 extends Position2D
 
 export(PackedScene) var Bala
+export(PackedScene) var Laser
 
 var angle = 0
 var direction = true
 var vectorDirection
 var vectorPosition
-var timer =0
-
+var timer = 0
+var timer2 = 0
+var counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,34 +18,63 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	timer+=1
+	if timer2 ==0:
+		if angle<=-0.3:
+			direction = true
+		if angle>=0.4:
+			direction = false
+			 
+		if direction:
+			rotate(0.01)
+			angle += 0.01
+		if !direction:
+			rotate(-0.01)
+			angle -= 0.01
 	
-	if angle<=-0.3:
-		direction = true
-	if angle>=0.4:
-		direction = false
-		 
-	if direction:
-		rotate(0.01)
-		angle += 0.01
-	if !direction:
-		rotate(-0.01)
-		angle -= 0.01
-	
-	if timer==10 || timer==17 || timer==24:
+		if timer==10 || timer==17 || timer==24:
+			var bala_instance = Bala.instance()
+			vectorPosition = $Sprite2/PositionBullet.global_position - global_position
+			bala_instance.pos = vectorPosition
+			bala_instance.pos.y -= 190
+			bala_instance.pos.x += 30
+
+			vectorDirection = $Sprite2/PositionBullet.global_position
+			vectorDirection.x = $Sprite2/PositionBullet.global_position.x - global_position.x
+			vectorDirection.y = $Sprite2/PositionBullet.global_position.y - global_position.y
+			
+			bala_instance.dir = vectorDirection
 		
-		var bala_instance = Bala.instance()
-		vectorPosition = $Sprite2/PositionBullet.global_position - global_position
-		bala_instance.pos = vectorPosition
-		bala_instance.pos.y -= 190
-		bala_instance.pos.x += 30
-		print(vectorPosition)
-		vectorDirection = $Sprite2/PositionBullet.global_position
-		vectorDirection.x = $Sprite2/PositionBullet.global_position.x - global_position.x
-		vectorDirection.y = $Sprite2/PositionBullet.global_position.y - global_position.y
-		
-		bala_instance.dir = vectorDirection
-		
-		get_parent().add_child(bala_instance)
+			get_parent().add_child(bala_instance)
 	if timer>60:
 		timer = 0
+		counter+=1
+		
+	if counter == 5:
+		timer2 = 1
+		if angle!=0:
+			if angle<=-0.3:
+				direction = true
+			if angle>=0.4:
+				direction = false
+			 
+			if direction:
+				rotate(0.01)
+				angle += 0.01
+			if !direction:
+				rotate(-0.01)
+				angle -= 0.01
+		
+		
+	if timer2 == 1:
+		var laser_instance = Laser.instance()
+		laser_instance.pos = Vector2(-75,-134)
+		laser_instance.add_collision_exception_with(self) 
+		print(vectorPosition)
+		get_parent().add_child(laser_instance)
+		counter+=1
+		if counter==100:
+			timer2=0
+			timer = 0
+			counter=0
+			direction = true
 	pass
---
+++ scenes/Laser.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+
+export(float) var LASER_VELOCITY = 500
+
+var dir = Vector2(-1,0)
+var pos = Vector2()
+var motion = Vector2(1, 0)
+func _ready():
+	position = pos
+	
+
+func _physics_process(delta):
+	motion = dir.normalized() * LASER_VELOCITY * delta
+	var collision = move_and_collide(motion)
+	
+	if (collision):
+		if (collision.collider.is_in_group("player")):
+			collision.collider.hit()
+		get_parent().remove_child(self)
+		
+
