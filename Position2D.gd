f4a99e0bc306b3aadf8a1613b9e32df33225343d
emilioeigler <emilioeigler@hotmail.com>
2018-11-21 07:42:12 -0300

    add class fish_state

--- Position2D.gd
+++ Position2D.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export(PackedScene) var Bala
 export(PackedScene) var Laser
+export(PackedScene) var Fish
 
 var angle = 0
 var direction = true
@@ -38,8 +39,8 @@ func set_state(new_state):
 		state = LaserState.new(self)
 	#elif new_state == STATE_KICK:
 	#	state = KickState.new(self)
-	#elif new_state == STATE_FISH:
-	#	state = FishState.new(self)
+	elif new_state == STATE_FISH:
+		state = FishState.new(self)
 	
 	pass
 	
@@ -83,14 +84,15 @@ class ShotState:
 		
 		if boss.timer>60:
 			boss.timer = 0
-		if timer2 >= 200:
+		if timer2 >= 300:
 			boss.set_state(boss.STATE_LASER)
-			
+			timer2 = 0
 		pass
 	
 	func _exit():
 		pass
 		
+# class LaserState---------------------------------------------------------------------------
 class LaserState:
 	var boss
 	var timer2 = 0
@@ -100,7 +102,8 @@ class LaserState:
 		pass
 	
 	func _fixed_process(delta):
-		timer2 +=1
+		
+		print(timer2)
 		if boss.angle!=0.2:
 			if boss.angle<=-0.3:
 				boss.direction = true
@@ -118,12 +121,66 @@ class LaserState:
 			laser_instance.pos = Vector2(-72,-130)
 			
 			boss.get_parent().add_child(laser_instance)
-		if timer2 >= 200:
-			boss.set_state(boss.STATE_SHOT)
+			timer2 +=1
+		if timer2 >= 50:
+			boss.set_state(boss.STATE_FISH)
+			timer2 = 0
 		pass
 	
 	func _exit():
 		pass
 	
+# class fishstate-----------------------------------------------------------------------------
+class FishState:
+	var boss
+	var timerFish = 0
+	func _init(boss):
+		self.boss = boss
+		
+		pass
 	
+	func _fixed_process(delta):
+		boss.timer+=1
+		
+		timerFish +=1
+		if boss.angle<=-0.3:
+			boss.direction = true
+		if boss.angle>=0.4:
+			boss.direction = false
+			 
+		if boss.direction:
+			boss.rotate(0.01)
+			boss.angle += 0.01
+		if !boss.direction:
+			boss.rotate(-0.01)
+			boss.angle -= 0.01
+		if boss.timer==10 || boss.timer==17 || boss.timer==24:
+			var bala_instance = boss.Bala.instance()
+			boss.vectorPosition = boss.get_node("Sprite2/PositionBullet").global_position + boss.position - boss.get_node("Sprite2").global_position
+			bala_instance.pos = boss.vectorPosition
+			bala_instance.pos.x = boss.get_node("Sprite2").position.x *2.5 
+	
+			boss.vectorDirection = boss.get_node("Sprite2/PositionBullet").global_position
+			boss.vectorDirection.x = boss.get_node("Sprite2/PositionBullet").global_position.x - boss.global_position.x
+			boss.vectorDirection.y = boss.get_node("Sprite2/PositionBullet").global_position.y - boss.global_position.y
+			
+			bala_instance.dir = boss.vectorDirection
+			boss.get_parent().add_child(bala_instance)
+		
+		if timerFish == 10 ||  timerFish == 25 || timerFish == 40 || timerFish == 55 || timerFish == 70:
+			var fish_instance = boss.Fish.instance()
+			boss.vectorPosition = Vector2(1024,rand_range(-201,200))
+			fish_instance.pos = boss.vectorPosition
+			boss.get_parent().add_child(fish_instance)
+			
+		print(timerFish)
+		if boss.timer>60:
+			boss.timer = 0
+		if timerFish >= 300:
+			boss.set_state(boss.STATE_SHOT)
+			timerFish = 0
+		pass
+	
+	func _exit():
+		pass
 	
